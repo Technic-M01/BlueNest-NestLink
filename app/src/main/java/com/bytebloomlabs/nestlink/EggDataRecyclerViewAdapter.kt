5cfce714d3a7d675a10a313b308953ae77a5092e
TechnicM01 <mauriciohvvilla1@gmail.com>
2023-12-02 14:00:22 -0600

    added drawables for egg types and logic to set icons for data points

--- app/src/main/java/com/bytebloomlabs/nestlink/EggDataRecyclerViewAdapter.kt
+++ app/src/main/java/com/bytebloomlabs/nestlink/EggDataRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package com.bytebloomlabs.nestlink
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView
 class EggDataRecyclerViewAdapter(
     private val values: MutableList<UserData.EggDataPoints>?): RecyclerView.Adapter<EggDataRecyclerViewAdapter.ViewHolder>() {
 
+    private lateinit var mContext: Context
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.content_egg_data, parent, false)
 
@@ -19,25 +22,40 @@ class EggDataRecyclerViewAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = values?.get(position)
 
+        val ctx: Context = holder.itemView.context
+
+        val iconRes = when (item?.eggType) {
+            ctx.getString(R.string.egg_type_temp) -> R.drawable.ic_egg_temp
+            ctx.getString(R.string.egg_type_weather) -> R.drawable.ic_egg_weather
+            "TestEgg" -> R.drawable.ic_egg_light
+
+            else -> R.drawable.ic_egg_undefined
+        }
+
+
         val telem = item?.telemetry?.toString(Charsets.UTF_8)
 
-
-        holder.idView.text = item?.id
+        holder.eggTypeIconIv.setImageResource(iconRes)
+        holder.idTv.text = item?.id
         //ToDo add logic for parsing telemetry
-        holder.telemetryView.text = telem
-        holder.telemetryTimestampView.text = item?.telemetryTimestamp
-        holder.eggTypeView.text = item?.eggType
+        holder.telemetryTv.text = telem
+        holder.telemetryTimestampTv.text = item?.telemetryTimestamp
+        holder.eggTypeTv.text = item?.eggType
     }
 
     override fun getItemCount(): Int = values?.size ?: 0
 
+    private fun setContext(ctx: Context) {
+        mContext = ctx
+    }
+
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val imageView: ImageView = view.findViewById(R.id.image)
-        val idView: TextView = view.findViewById(R.id.id)
-        val telemetryView: TextView = view.findViewById(R.id.telemetry)
-        val telemetryTimestampView: TextView = view.findViewById(R.id.telemetry_timestamp)
-        val eggTypeView: TextView = view.findViewById(R.id.egg_type)
+        val eggTypeIconIv: ImageView = view.findViewById(R.id.iv_egg_type_icon)
+        val idTv: TextView = view.findViewById(R.id.tv_id)
+        val telemetryTv: TextView = view.findViewById(R.id.tv_telemetry)
+        val telemetryTimestampTv: TextView = view.findViewById(R.id.tv_telemetry_timestamp)
+        val eggTypeTv: TextView = view.findViewById(R.id.tv_egg_type)
     }
 
 }
